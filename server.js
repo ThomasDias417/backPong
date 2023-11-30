cb9c68f44f7c961751fbb5c13afb58744c83935a
thomas <thomasdias417@gmail.com>
2023-11-30 21:40:45 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const {Server} = require('socket.io');
+const port = process.env.PORT || 8000
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET","POST"],
+    },
+});
+
+io.on('connect',(socket)=>{
+    console.log('user connected ',socket.id)
+    Farkel(socket)
+})
+
+server.listen(port,()=>{
+    console.log(`listening on ${port}`);
+})
